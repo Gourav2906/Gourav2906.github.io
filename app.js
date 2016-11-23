667058d46342a1754b28bfd9ddfea77a6251399c
gourav2906 <ggarg2906@gmail.com>
2016-11-24 00:18:00 +0530

    first page

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
- var myApp= angular.module('BlankApp', ['ngMaterial' ,'jkAngularCarousel']);
+ var myApp= angular.module('BlankApp', ['ngMaterial' ,'ngRoute','jkAngularCarousel']);
+
+  myApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "./views/home.html"
+    })
+    .when("/products", {
+        templateUrl : "./views/products.html",
+    })
+  });
+
 
     myApp.controller('MyCtrl', function($scope) {
     $scope.dataArray = [
       {
-        src: 'http://www.parasholidays.in/blog/wp-content/uploads/2014/05/holiday-tour-packages-for-usa.jpg'
+        // src: 'http://www.parasholidays.in/blog/wp-content/uploads/2014/05/holiday-tour-packages-for-usa.jpg'
+        src:'pic1.jpg'
       },
       {
-        src: 'http://clickker.in/wp-content/uploads/2016/03/new-zealand-fy-8-1-Copy.jpg'
+        src: 'pic2.jpg'
       },
       {
-        src: 'http://images.kuoni.co.uk/73/indonesia-34834203-1451484722-ImageGalleryLightbox.jpg'
+        src: 'pic3.jpg'
       },
       {
-        src: 'http://images.kuoni.co.uk/73/malaysia-21747826-1446726337-ImageGalleryLightbox.jpg'
+        src: 'pic4.jpg'
       },
       {
-        src: 'http://www.kimcambodiadriver.com/uploads/images/tours/kim-cambodia-driver-angkor-wat.jpg'
+        src: 'pic5.jpg'
       },
-      {
-        src: 'https://www.travcoa.com/sites/default/files/styles/flexslider_full/public/tours/images/imperialvietnam-halong-bay-14214576.jpg?itok=O-q1yr5_'
-      }
+      
     ];
 });
+    
+myApp.controller('toolbarCtrl', function($scope,$location) {
+
+        $scope.goto = function (val) {
+        $location.path(val);
+    };
+
+    });
